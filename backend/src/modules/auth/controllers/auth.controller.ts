f1651f21d3dd4038363fd1745fc36ba3cade9d5b
Micaela Gomez <mica16gomez@gmail.com>
2026-05-20 22:47:47 -0300

    Primer commit del proyecto integrador

---
+++ backend/src/modules/auth/controllers/auth.controller.ts
@@ -0,0 +1,16 @@
+import { BadRequestException, Body, Controller, NotImplementedException, Post } from "@nestjs/common";
+import { LoginDto } from "../dtos/input/login.dto";
+import { AuthService } from "../services/auth.service";
+
+@Controller("auth")
+export class AuthController{
+
+    constructor(private readonly authService: AuthService){}
+
+    @Post("")
+    async login(@Body() dto: LoginDto): Promise<{accessToken: string}>{
+        return await this.authService.login(dto);
+    }
+
+
+}
---
+++ backend/src/modules/gestion/dtos/output/list-cliente.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { EstadosClientesEnum } from "../../enums/estados-clientes.enum";
+
+export class ListClienteDTO {
+
+    @ApiProperty()
+    id!: number;
+
+    @ApiProperty()
+    nombre!: string;
+
+    @ApiProperty()
+    estado!: EstadosClientesEnum;
+
+}
+
